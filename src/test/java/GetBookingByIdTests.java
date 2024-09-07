24931d175407792c84235b4ca279a6156101bf6e
JavaReview <eaasbb@gmail.com>
2024-09-07 13:37:56 +0300

    GetBookingByIdAsserstions

--- src/test/java/GetBookingByIdTests.java
+++ src/test/java/GetBookingByIdTests.java
@@ -1,3 +1,5 @@
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,16 +13,26 @@ public class GetBookingByIdTests {
         // Create call
         // Check response
         // https://restful-booker.herokuapp.com/booking/1
-given()
+Response response = given()
         .when()
-        .get("https://restful-booker.herokuapp.com/booking/203")
-        .then()
-        .log().all()
-        .statusCode(200);
+        .get("https://restful-booker.herokuapp.com/booking/633");
+
+//        .log().all()
+//        .statusCode(200);
+
+        response
+                .then()
+                .statusCode(200);
+
+       response.prettyPrint();
+
+        String firstname = response.jsonPath().getJsonObject("firstname");
+        String lastname = response.jsonPath().getJsonObject("lastname");
+        int totalprice = response.jsonPath().getJsonObject("totalprice");
+
+        Assertions.assertEquals("Josh", firstname);
+        Assertions.assertEquals("Allen", lastname);
+        Assertions.assertEquals(111, totalprice);
 
     }
-
-
-
-
 }
